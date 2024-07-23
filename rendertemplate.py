9c44bcd880bd081d7e79c6f48691c574f3c302e1
David Nanjila <nanjiladavid2@gmail.com>
2024-07-23 08:30:11 +0300

    feat: write a program to send data to result page

--- rendertemplate.py
+++ rendertemplate.py
@@ -1,8 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('home.html')
+
+@app.route('/results', methods=['POST', 'GET'])
+def results():
+    if request.method == 'POST':
+        result = request.form
+        return render_template('result.html', result=result)
+
 
 if __name__== '__main__':
     app.run(port='5001', debug=True)
