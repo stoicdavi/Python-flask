023c6a8a4f50b6663d9da709062d5abae50a75a9
David Nanjila <nanjiladavid2@gmail.com>
2024-07-23 19:26:16 +0300

    create a simple script to demonstrate flask message flashing
    Signed-off-by: David Nanjila <nanjiladavid2@gmail.com>

---
+++ messageFlashing/flash.py
@@ -0,0 +1,21 @@
+from flask import Flask, redirect, flash, render_template, request, url_for
+app = Flask(__name__)
+app.secret_key = 'ravdavi'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or \
+            request.form['password'] != 'admin':
+            error = 'Invalid username or password. Please try again!'
+        else:
+            flash('You were successfully logged in')
+            flash('log out before login again')
+            return redirect(url_for('index'))
+    return render_template('log_in.html', error=error)
+if __name__ == '__main__':
+    app.run(port='5004', debug=True)
